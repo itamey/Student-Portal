f05e3bbda7c018e440d7ddcc27b0f3c4a898c663
Shalom <eyoshalom@yahoo.com>
2022-08-04 00:01:31 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+from cgitb import html
+from flask import Flask, render_template, url_for
+import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/dashboard')
+def about():
+    return render_template("dashboard.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
